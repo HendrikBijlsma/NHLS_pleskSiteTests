168fdcf556cf1372801aa6eedd8083ac60515446
HendrikBijlsma <87723691+HendrikBijlsma@users.noreply.github.com>
2021-07-20 21:58:46 +0200

    Create index.php
    First test created

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Gtihub gemaakte testpagina voor combinatie met plesk NHL Stenden website
+
+?>
+
+<HTML>
+  <HEAD>
+    <TITLE>Test pagina</TITLE>
+  </HEAD>
+  <BODY>
+    <CENTER>Test body</CENTER>
+    <?php
+    printf("<H1 style='text-align: center'>PHP werkt!</H1>");
+    ?>
+  </BODY>
+</HTML>
+  
